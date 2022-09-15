650d6aba85822c7baad19a99f182eb26331f6ee4
Jei Ann M. Bayer <22100000@usc.edu.ph>
2022-09-15 20:25:50 +0800

    added file

---
+++ commisionEmployee.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+import static java.lang.System.out;
+
+public class commisionEmployee {
+
+    private int empIDcom;
+    private String empNamecom;
+    private double totalSales;
+
+    public int getEmpIDcom() {
+        return empIDcom;
+    }
+
+    public String getEmpNamecom() {
+        return empNamecom;
+    }
+
+    public double getTotalSales() {
+        return totalSales;
+    }
+
+    public void setEmpIDcom(int empIDcom) {
+        this.empIDcom += empIDcom;
+    }
+
+    public void setEmpNamecom(String empNamecom) {
+        this.empNamecom = empNamecom;
+    }
+
+    public void setTotalSales(double totalSales) {
+        this.totalSales += totalSales;
+    }
+
+    public void getDisplayInfocom() {
+        out.println("COMMISION EMPLOYEE DETAILS: ");
+        out.println("");
+        out.println("COMMISION EMPLOYEE ID: " + getEmpIDcom());
+        out.println("COMMISION EMPLOYEE NAME: " + getEmpNamecom());
+        out.println("TOTAL SALES: " + getTotalSales());
+    }
+
+    public void getCommsionSalary() {
+        double j;
+        if (getTotalSales() <= 10000) {
+            j = getTotalSales() * .05;
+            out.println("COMPUTED COMMISION OF AN EMPLOYEE: " + j);
+        } else if (getTotalSales() > 10000 && getTotalSales() <= 50000) {
+            j = getTotalSales() * .10;
+            out.println("COMPUTED COMMISION OF AN EMPLOYEE: " + j);
+        } else if (getTotalSales() > 50000 && getTotalSales() <= 100000) {
+            j = getTotalSales() * .15;
+            out.println("COMPUTED COMMISION OF AN EMPLOYEE: " + j);
+        } else if (getTotalSales() > 100000) {
+            j = getTotalSales() * .30;
+            out.println("COMPUTED COMMISION OF AN EMPLOYEE: " + j);
+        }
+
+    }
+}
