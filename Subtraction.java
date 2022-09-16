a02e55929b65a89ac88d1913879702b77662e0f0
John <21103870@usc.edu.ph>
2022-09-16 14:53:41 +0800

    added code in subtraction file

---
+++ Subtraction.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main
+
+
+{
+	public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("Enter first number");
+    int numA = input.nextInt();
+
+    System.out.println("Enter second number");
+    int numB = input.nextInt();
+    
+    System.out.println(numA - numB);
+
+
+    
+	}
+}
