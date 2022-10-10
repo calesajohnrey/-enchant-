78287e27a7cc2cb194f1edbb3fec786f9ceeba77
John <21103870@usc.edu.ph>
2022-10-11 07:37:35 +0800

    new updates

--- OOP Folder/OOP_File.java
+++ OOP Folder/OOP_File.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Classes;
-
 import static java.lang.System.out;
 
 // This is JOHN REY CALESA'S code
-public class hourlyEmployee {
+public class OOP_File {
 
     private int empID;
     private String empName;
